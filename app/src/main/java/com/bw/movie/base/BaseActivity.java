3c331176cc319bd1f029151b530e348fe3bee26e
Anna <2103186530@qq.com>
2019-01-03 15:03:32 +0800

    base完善

--- app/src/main/java/com/bw/movie/base/BaseActivity.java
+++ app/src/main/java/com/bw/movie/base/BaseActivity.java
@@ -3,6 +3,9 @@ package com.bw.movie.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.bw.movie.R;
 import com.bw.movie.util.StatusBarUtil;
 
@@ -11,6 +14,7 @@ import com.bw.movie.util.StatusBarUtil;
 * */
 public abstract class BaseActivity<T extends BasePresenter> extends AppCompatActivity {
     private T mBasePresenter;
+    private StatusView statusView;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,8 +27,6 @@ public abstract class BaseActivity<T extends BasePresenter> extends AppCompatAct
             initData();
         } else {
             finish();
-
-
         }
     }
 
@@ -46,7 +48,34 @@ public abstract class BaseActivity<T extends BasePresenter> extends AppCompatAct
         StatusBarUtil.setStatusBarColor(color);
     }
 
+    @Override
+    public void setContentView(View view) {
+        super.setContentView(view);
+        statusView=initStatuView(view);
+    }
 
+    @Override
+    public void setContentView(int layoutResID) {
+        View inflate = View.inflate(this, layoutResID, null);
+        statusView=initStatuView(inflate);
+        super.setContentView(layoutResID);
+    }
+
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        statusView=initStatuView(view);
+        super.setContentView(view, params);
+    }
+
+    private StatusView initStatuView(View content) {
+        StatusView.Builder builder = new StatusView.Builder(this);
+        statusView = builder.contentView(content)
+                .emptyId(R.layout.layout_empity2)
+                .erroryId(R.layout.layout_error)
+                .loadingId(R.layout.layout_loading)
+                .build();
+        return statusView;
+    }
     @Override
     public void onAttachedToWindow() {
         super.onAttachedToWindow();
