6ab79d59cd4c3e3116976b6af2fc9368b2a36317
张文龙 <123>
2019-01-07 14:36:44 +0800

    message changed fourth

--- app/src/main/java/com/bw/movie/my/MyHeadportraitActivity.java
+++ app/src/main/java/com/bw/movie/my/MyHeadportraitActivity.java
@@ -4,12 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.bw.movie.R;
+import com.bw.movie.base.BaseActivity;
+import com.bw.movie.base.BasePresenter;
 
-public class MyHeadportraitActivity extends AppCompatActivity {
+public class MyHeadportraitActivity extends BaseActivity {
+    @Override
+    public void initView() {
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_headportrait);
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public int initLayoutId() {
+        return R.layout.activity_headportrait;
+    }
+
+    @Override
+    public void initVariable() {
+
+    }
+
+    @Override
+    public BasePresenter initPresenter() {
+        return null;
     }
 }
