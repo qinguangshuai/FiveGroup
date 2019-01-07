7b419a66e14adf43e80b73b3f89f8f73723fa03f
张文龙 <123>
2019-01-07 10:22:49 +0800

    message changed second

--- app/src/main/java/com/bw/movie/film/event/PraiseEvent.java
+++ app/src/main/java/com/bw/movie/film/event/PraiseEvent.java
@@ -1,5 +1,7 @@
 package com.bw.movie.film.event;
 
+import android.widget.CheckBox;
+
 /*
  *作者:ash
  *TODO:
@@ -7,6 +9,27 @@ package com.bw.movie.film.event;
  */
 public class PraiseEvent {
     private int i ;
+    private CheckBox checkBox;
+    private int num ;
+
+    public int getNum() {
+        return num;
+    }
+
+    public PraiseEvent(int i, CheckBox checkBox, int num) {
+        this.i = i;
+        this.checkBox = checkBox;
+        this.num = num;
+    }
+
+    public CheckBox getCheckBox() {
+        return checkBox;
+    }
+
+    public PraiseEvent(int i, CheckBox checkBox) {
+        this.i = i;
+        this.checkBox = checkBox;
+    }
 
     public int getI() {
         return i;
